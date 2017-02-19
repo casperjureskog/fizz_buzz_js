d7c7043277ea1a0c3f53f62235b2d37d45a16cd1
casperjureskog <lcasper@mac.com>
2017-02-19 01:24:41 +0100

    add a corection to if statment for out of rannge numbers

--- assets/js/fizz_calulator.js
+++ assets/js/fizz_calulator.js
@@ -4,7 +4,7 @@ function FIZZCalculator(){
 FIZZCalculator.prototype.fizz_buzz = function(obj) {
   var player1 = obj.player1;
 
-  if (player1 < 0) {
+  if (player1 <= 0) {
     obj.fizzMessage = "wrong input"
   } else if (player1 % 15 === 0) {
     obj.fizzMessage = "fizzbuzz"
@@ -12,6 +12,8 @@ FIZZCalculator.prototype.fizz_buzz = function(obj) {
     obj.fizzMessage = "buzz"
   }else if (player1 % 3 === 0) {
     obj.fizzMessage = "fizz"
+  }else{
+    obj.fizzMessage = "not dividible by 3 or 5"
   }
 
 

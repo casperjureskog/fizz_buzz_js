ad107e6f1b08cd066d40c31704b7be64900525d4
casperjureskog <lcasper@mac.com>
2017-02-19 01:17:45 +0100

    add base funktionality

--- assets/js/fizz_calulator.js
+++ assets/js/fizz_calulator.js
@@ -9,9 +9,9 @@ FIZZCalculator.prototype.fizz_buzz = function(obj) {
   } else if (player1 % 15 === 0) {
     obj.fizzMessage = "fizzbuzz"
   }else if (player1 % 5 === 0) {
-    obj.fizzMessage = "Overweight"
+    obj.fizzMessage = "buzz"
   }else if (player1 % 3 === 0) {
-    obj.fizzMessage = "Obese"
+    obj.fizzMessage = "fizz"
   }
 
 
